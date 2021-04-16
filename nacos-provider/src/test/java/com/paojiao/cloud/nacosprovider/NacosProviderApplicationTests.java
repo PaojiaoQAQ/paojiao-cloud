73010053d79f0af0fb8f89a6cbdbc40f7d819403
tanyue <paojiao1224@163.com>
2021-04-16 17:18:35 +0800

    nacos注册中心生产者

---
+++ nacos-provider/src/test/java/com/paojiao/cloud/nacosprovider/NacosProviderApplicationTests.java
@@ -0,0 +1,15 @@
+package com.paojiao.cloud.nacosprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosProviderApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
